b24b5faa2036ef592e2a3448ee2d6f6500feb47a
Daniel Beard <Daniel@promenadesoftware.com>
2019-12-14 21:30:02 -0800

    Added slow flash script for my wierd ubuntu issue

---
+++ slow_flash.sh
@@ -0,0 +1,3 @@
+# Ubuntu messes up the fast flashing, so we can manually flash the esp32 at 115200 . It's way slower but at least it works *shrug*
+"/home/dan/.platformio/penv/bin/python" "/home/dan/.platformio/packages/tool-esptoolpy/esptool.py" --chip esp32 --port "/dev/ttyUSB0" --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 /home/dan/.platformio/packages/framework-arduinoespressif32/tools/sdk/bin/bootloader_dio_40m.bin 0x8000 "/home/dan/Projects/oss/space force/sf_ensign_fw_0.2.7a/.pio/build/space-force-ensign-rev3/partitions.bin" 0xe000 /home/dan/.platformio/packages/framework-arduinoespressif32/tools/partitions/boot_app0.bin 0x10000 .pio/build/internet-of-batteries/firmware.bin 
+
